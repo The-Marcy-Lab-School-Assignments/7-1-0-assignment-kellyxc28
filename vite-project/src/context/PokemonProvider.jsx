6db3339bc3e78a9a9feb5a9bfcda744139ec0f59
kellyxc28 <kellyxiongc@gmail.com>
2024-08-20 08:36:44 -0400

    Attempted: Half of the First Feature (Steps 1-4)

--- vite-project/src/context/PokemonProvider.jsx
+++ vite-project/src/context/PokemonProvider.jsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import handleFetch from '../utils/handleFetch';
-
 // TODO: Import the PokemonContext
+import PokemonContext from './PokemonContext';
 
 const starterPokemon = [
     {
@@ -29,16 +29,36 @@ const starterPokemon = [
 
 const PokemonProvider = ({ children }) => {
     const [allPokemon, setAllPokemon] = useState(starterPokemon);
+    const [error, setError] = useState();
 
     // TODO: use useEffect to fetch data from the local JSON server (remember to start JSON server!)
+    useEffect(() => {
+        const doFetch = async () => {
+        //   const [data, error] = await handleFetch(query ? `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${query}&limit=3&rating=g`: `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=3&rating=g`);
+          const [data, error] = await handleFetch('http://localhost:4000/pokemon');
+          if (data) setAllPokemon(data.data);
+          if (error) setError(error);
+        };
+        doFetch();
+      }, []);
+    
+      useEffect(() => {
+        console.log(allPokemon);
+        // console.log(pokemon); // 'pokemon' is undefined 
+        // console.log('Hello!');
+      }, [allPokemon])
+    
+      if (error) return <p>{error.message}</p>
 
     // TODO: Add values to be included in the context here
-    let contextValues = {}
+    let contextValues = { allPokemon, setAllPokemon };
 
     // TODO: Wrap the {children} in the PokemonContext.Provider and provide the values above
     return (
-        { children }
-    )
+        <PokemonContext.Provider value={contextValues}>
+            { children }
+        </PokemonContext.Provider>
+    );
 }
 
 export default PokemonProvider;
